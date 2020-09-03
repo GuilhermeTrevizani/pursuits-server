ff0c70e760d663278bef6e0ec022ccb87ab04301
Guilherme Trevizani <tr3v1z4@gmail.com>
2020-09-03 18:44:16 -0300

    Exibir quantidade de jogadores do Discord not BOT

--- LesterBOT/Program.cs
+++ LesterBOT/Program.cs
@@ -3,6 +3,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,24 +12,43 @@ namespace LesterBOT
 {
     class Program
     {
+        DiscordSocketClient Client { get; set; }
+        ulong GuildId { get; } = 739257198353580112;
+
         static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
             using (var services = ConfigureServices())
             {
-                var client = services.GetRequiredService<DiscordSocketClient>();
+                Client = services.GetRequiredService<DiscordSocketClient>();
 
-                client.Log += LogAsync;
+                Client.Log += LogAsync;
+                Client.UserJoined += Client_UserJoined;
+                Client.UserLeft += Client_UserLeft;
+                Client.Ready += Client_Ready;
                 services.GetRequiredService<CommandService>().Log += LogAsync;
 
-                await client.LoginAsync(TokenType.Bot, "NzUwNjkxODQxNDUxNzUzNTEz.X0-OQg.ZDhhumvOiaE9gfPZoPy4JLL6oPo");
-                await client.StartAsync();
-
-                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+                await Client.LoginAsync(TokenType.Bot, "NzUwNjkxODQxNDUxNzUzNTEz.X0-OQg.ZDhhumvOiaE9gfPZoPy4JLL6oPo");
+                await Client.StartAsync();
 
                 await Task.Delay(Timeout.Infinite);
             }
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync($"{Client.Guilds.FirstOrDefault(x => x.Id == GuildId)?.Users.Count} jogadores", type: ActivityType.Listening);
+        }
+        
+        private async Task Client_UserLeft(SocketGuildUser arg)
+        {
+            await Client.SetGameAsync($"{Client.Guilds.FirstOrDefault(x => x.Id == GuildId)?.Users.Count} jogadores", type: ActivityType.Listening);
+        }
+
+        private async Task Client_UserJoined(SocketGuildUser arg)
+        {
+            await Client.SetGameAsync($"{Client.Guilds.FirstOrDefault(x => x.Id == GuildId)?.Users.Count} jogadores", type: ActivityType.Listening);
         }
 
         private Task LogAsync(LogMessage log)
